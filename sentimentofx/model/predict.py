adf121f05913f4a8e54065be9b53d6bfc553680a
rsradev <raddy.radev@gmail.com>
2025-07-27 18:36:13 +0300

    main and fundamentals methods

--- sentimentofx/model/predict.py
+++ sentimentofx/model/predict.py
@@ -1,10 +1,17 @@
 from transformers import pipeline
 from huggingface_hub import login
+import os
+from dotenv import load_dotenv
 
-login(token="")
 
+load_dotenv()
 
-sentiment_pipeline = pipeline("sentiment-analysis")
+login(token=os.getenv("HUGGINGFACE_TOKEN"))
+
+sentiment_pipeline = pipeline(
+    "sentiment-analysis"
+
+)
 
 def predict_sentiment(text: str) -> dict:
     result = sentiment_pipeline(text)[0]
